ab3892cc1fbb9615f18ae81c17ae0b1c6c71d407
Martin Freire <martin.vallejo01@gmail.com>
2018-11-28 12:11:30 -0500

    Created Pet class and some pages to tabbed pages

---
+++ PetsCare/PetsCare/Models/Pet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetsCare.Models
+{
+    public class Pet
+    {
+        public int PetID { get; set; }
+        public String Name { get; set; }
+        public String Breed { get; set; }
+        public String Colors { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public String AllergiesAndIllness { get; set; }
+        public String Comments { get; set; }
+
+        // public Clinic Clinic { get; set; }
+        // public Veterinarian Vet { get; set }
+    }
+
+    public enum Gender { Male = 0, Female = 1}
+}
