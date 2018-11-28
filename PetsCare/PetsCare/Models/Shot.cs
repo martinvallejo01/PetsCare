92a8c84c22ac20b01e5a784b7383bbfb16e76f63
Martin Freire <martin.vallejo01@gmail.com>
2018-11-28 12:44:55 -0500

    Class shot and shot information

---
+++ PetsCare/PetsCare/Models/Shot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetsCare.Models
+{
+    class Shot
+    {
+        public int ShotID { get; set; }
+        public ShotInformation ShotInformation { get; set; }
+        public DateTime NextVaccine { get; set; }
+    }
+}
---
+++ PetsCare/PetsCare/Models/ShotInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetsCare.Models
+{
+    public class ShotInformation
+    {
+        public int ShotID { get; set; }
+        public String Name { get; set; }
+        public String Info { get; set; }
+    }
+}
