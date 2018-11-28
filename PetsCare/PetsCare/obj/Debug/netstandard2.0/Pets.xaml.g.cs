007f55db8166464c8681a9669837fdd37ed9f61a
Martin Freire <martin.vallejo01@gmail.com>
2018-11-28 12:28:11 -0500

    Pets list view in pets page

--- PetsCare/PetsCare/obj/Debug/netstandard2.0/Pets.xaml.g.cs
+++ PetsCare/PetsCare/obj/Debug/netstandard2.0/Pets.xaml.g.cs
@@ -8,17 +8,21 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Xamarin.Forms.Xaml.XamlResourceIdAttribute("PetsCare.Pets.xaml", "Pets.xaml", typeof(global::PetsCare.Pets))]
+[assembly: global::Xamarin.Forms.Xaml.XamlResourceIdAttribute("PetsCare.Pets.xaml", "Pets.xaml", typeof(global::PetsCare.PetsPage))]
 
 namespace PetsCare {
     
     
     [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Pets.xaml")]
-    public partial class Pets : global::Xamarin.Forms.ContentPage {
+    public partial class PetsPage : global::Xamarin.Forms.ContentPage {
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.ListView petsListView;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
-            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Pets));
+            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(PetsPage));
+            petsListView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "petsListView");
         }
     }
 }
