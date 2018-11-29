5392ce19047f7000754b64550c6960d2e380accc
Martin Freire <martin.vallejo01@gmail.com>
2018-11-29 09:01:55 -0500

    VetsPage, VetPage, ClincPage, ClinicsPage

---
+++ PetsCare/PetsCare/ClinicDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using PetsCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PetsCare
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ClinicDetailPage : ContentPage
+	{
+		public ClinicDetailPage (Clinic clinic)
+		{
+            if (clinic == null)
+                throw new ArgumentNullException();
+            BindingContext = clinic;
+
+			InitializeComponent ();
+		}
+	}
+}
---
+++ PetsCare/PetsCare/ClinicsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PetsCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PetsCare
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ClinicsPage : ContentPage
+	{
+		public ClinicsPage ()
+		{
+			InitializeComponent ();
+            clinicsListView.ItemsSource = new List<Clinic>
+            {
+                new Clinic {Name = "San Fransisco", Phone = "0997288893"},
+                new Clinic {Name = "Hospital de los Valles", Phone = "3820268"}
+            };
+		}
+
+        private async void ClinicsListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var clinic = e.SelectedItem as Clinic;
+            if (clinic == null)
+                return;
+            await Navigation.PushAsync(new ClinicDetailPage(clinic));
+            clinicsListView.SelectedItem = null;
+        }
+    }
+}
